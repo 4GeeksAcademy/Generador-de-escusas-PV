19c8fd4ab54b792204390b796ec63b54e31bb7b0
Pamelavignolo <136765465+Pamelavignolo@users.noreply.github.com>
2025-04-22 16:09:44 +0000

    trabajo terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  function elegirEscusa(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+  }
+
+  function generarEscusa() {
+    let excuse = elegirEscusa(who) + " " + elegirEscusa(action) + " " + elegirEscusa(what) + " " + elegirEscusa(when) + "." ;
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+ 
+  generarEscusa();
+  
 };
+
+
